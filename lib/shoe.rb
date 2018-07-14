bb4d6d589cd0858f08ea649f3176b6a4f284ae78
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-07-14 19:47:51 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    puts "The shoe has been repaired!:"
+  end
+end
